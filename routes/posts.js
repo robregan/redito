88129cb4381204abbed387f4a36bcc724f188df8
rob <robert_regan86@yahoo.com>
2022-03-04 15:35:21 -0500

    heyyo!

---
+++ routes/posts.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+
+router.get("/:id", ensureAuth, postsController.getPost);
+
+router.post("/createPost", upload.single("file"), postsController.createPost);
+
+router.put("/likePost/:id", postsController.likePost);
+
+router.put("/editPost/:id", postsController.editPost);
+
+router.delete("/deletePost/:id", postsController.deletePost);
+
+module.exports = router;
